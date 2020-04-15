ca146700405225e515693d3ae04d87f25b3f951d
Tosin Oni <tosinoni@Tosins-MacBook-Pro.local>
2020-04-14 22:36:59 -0400

    finished implementing available/selected options

--- screens/create/CreateItemThree.js
+++ screens/create/CreateItemThree.js
@@ -1,12 +1,43 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import OptionsList from "../../components/OptionsList";
 
+const availableOptionsList = [
+  "Leather Seats",
+  "Sunroof/Moonroof",
+  "Navigation System",
+  "DVD/Entertainment",
+  "Power Seats",
+  "Heated Seats",
+  "Backup Camera",
+  "Parking Sensors",
+  "Air Conditioning",
+  "Alloy Wheels",
+  "Power Windows",
+  "Keyless Entry",
+  "Cruise Control"
+];
 export class CreateItemThree extends Component {
+  state = {
+    availableOptions: availableOptionsList,
+    selectedOptions: []
+  };
+
+  onItemSelected = (availableOptions, selectedOptions) => {
+    this.setState({ availableOptions, selectedOptions });
+  };
+
   render() {
+    const { availableOptions, selectedOptions } = this.state;
+
     return (
-      <View>
-        <Text>Item 3</Text>
-      </View>
+      <OptionsList
+        initialText="AVAILABLE OPTIONS"
+        selectedText="SELECTED OPTIONS"
+        initialList={availableOptions}
+        selectedList={selectedOptions}
+        onItemSelected={this.onItemSelected}
+      />
     );
   }
 }
